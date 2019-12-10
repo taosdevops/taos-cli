1d94ca5253ed1f2cb81ed776a2d5057bee969f39
Adam McChesney <amcchesney@taos.com>
2019-12-10 11:05:48 -0600

    adding initial setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    author="TAOS DevopsNow",
+    name="taos",
+    description="Taos Command Line",
+    license=open("LICENSE").read(),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    version="1.0.0",
+    url="https://github.com/taosdevops/taos",
+    install_requires=[
+        "Click==7.0",
+    ],
+    entry_points="""
+        [console_scripts]
+        taos=taos.cli.main:main
+        """,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
