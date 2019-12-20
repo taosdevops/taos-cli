6937b03498b0b8de213ee4e8bc90f09ee88302aa
carissa <cmorrow@taos.com>
2019-12-20 10:47:55 -0700

    added author email for metadata

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/taosdevops/taos",
     install_requires=[
         "Click==7.0",
