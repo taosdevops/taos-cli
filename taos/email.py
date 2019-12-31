a4168aa690867d60b8432091f0f6857a603bb3d1
Adam McChesney <amcchesney@taos.com>
2019-12-31 12:59:39 -0600

    changed email to use a command pattern

--- taos/email.py
+++ taos/email.py
@@ -3,65 +3,44 @@ from sendgrid.helpers.mail import Mail
 from taos import config
 
 
-def _get_email_string(**kwargs):
-    return "<br>".join(
-        [
+def _get_email_string(message_type="contact", **kwargs):
+    _email_string_types = {
+        "contact":"<br>".join([
             "<strong>New Contact Request</strong>",
             "New Contact request from taos-cli for {service_type}",
             "Requested by {name} at {email}."
-        
-        ]
-    ).format(**kwargs)
-
-def _get_email_string2(**kwargs):
-    return "<br>".join(
-        [
+        ]),
+        "subscribe":"<br>".join([
             "<strong>New Contact Request</strong>",
             "New Contact request from taos-cli for {service_type}",
             "Requested by {name} at {email}.",
-            "For hours: {hours} , length of contract: {length}, preferred communication: {communication}"
-        
-        ]
-    ).format(**kwargs)
-      
+            ("For hours: {hours} , length of contract: {length},"
+             " preferred communication: {communication}")
+        ])
+    }
+    try:
+        return _email_string_types[message_type].format(**kwargs)
+    except KeyError: return ""
 
 
-def send_message(name, email, service_type):
+def send_message(email, name, message_type="contact", **kwargs):
+    if "mailto" in email:
+        email = email.split(':')[1].split('|')[0]
+    print(kwargs)
     message = Mail(
         from_email=email,
         to_emails=config.SUPPORT_EMAIL,
         subject=f"Contact Request from {name}",
         html_content=_get_email_string(
-            name=name, email=email, service_type=service_type
+            email=email, name=name, message_type=message_type ,**kwargs
         ),
     )
 
     try:
         sg = SendGridAPIClient(config.SEND_GRID_API_KEY)
         response = sg.send(message)
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
-    except Exception as e:
-        print(str(e))
-
-
-
-def send_message2(name, email, service_type, hours, length, communication ):
-    message = Mail(
-        from_email=email,
-        to_emails=config.SUPPORT_EMAIL,
-        subject=f"Contact Request from {name}",
-        html_content=_get_email_string2(
-            name=name, email=email, service_type=service_type, hours=hours, length=length, communication=communication
-        ),
-    )
-
-    try:
-        sg = SendGridAPIClient(config.SEND_GRID_API_KEY)
-        response = sg.send(message)
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
+        print("StatusCode:",response.status_code)
+        print("Body:",response.body)
+        print("Headers",response.headers)
     except Exception as e:
         print(str(e))
